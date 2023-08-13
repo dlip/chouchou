89a3b5104039bc69b22802d65d0f4c38aefabff6
Dane Lipscombe <danelipscombe@gmail.com>
2023-08-13 15:12:09 +1000

    add mods

--- firmware/kmk/kb.py
+++ firmware/kmk/kb.py
@@ -1,7 +1,6 @@
 import board
 from kmk.kmk_keyboard import KMKKeyboard
 from kmk.scanners.keypad import KeysScanner
-# from kmk.scanners import DiodeOrientation
 
 class ChouChou(KMKKeyboard):
 
@@ -11,14 +10,4 @@ class ChouChou(KMKKeyboard):
             board.GP12, board.GP13, board.GP14, board.GP15,   board.GP4, board.GP5, board.GP6, board.GP7,
                                     board.GP11, board.GP10,   board.GP9, board.GP8,
         ])
-
-    # coord_mapping = [
-    #  0,  1,  2,  3,   4,  5,  6,  7,
-    #  8,  9, 10, 11,  12, 13, 14, 15,
-    #         16, 17,  18, 19
-    # ]
-
-# keyboard.col_pins = (board.GP26,)
-# keyboard.row_pins = (board.GP1,)
-# keyboard.diode_orientation = DiodeOrientation.COL2ROW
 
